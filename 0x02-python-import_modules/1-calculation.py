cc60ed386dad9e27533d234f2516a347f9aa3f18
marvcv <librarianmarvin@gmail.com>
2023-11-30 06:28:16 -0800

    calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,9 @@
-# Check if this module is being run as the main module
+#!/usr/bin/python3
 if __name__ == "__main__":
-    # Import the add function from add_0 module
-    from add_0 import add
-
-    # Define two numbers to add
-    a = 1
-    b = 2
-
-    # Perform addition and display the result
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
