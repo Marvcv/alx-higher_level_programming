d21b00b2ab8c5eab3f5cdee38f37a26f8d9afd7a
marvcv <librarianmarvin@gmail.com>
2023-11-30 06:19:15 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-
-"""
-This script demonstrates the usage of an 'add' function imported from the 'add_0' module.
-It performs addition of two numbers and prints the result.
-"""
-
 if __name__ == "__main__":
     from add_0 import add
-
     a = 1
     b = 2
-
     print("{} + {} = {}".format(a, b, add(a, b)))
