e260a089232e75153c09397a1ed20baa9644552c
marvcv <librarianmarvin@gmail.com>
2024-01-08 23:49:39 -0800

    lst

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,12 +5,3 @@ def is_same_class(obj, a_class):
     True: If object is exactly an instance of specified class
     False: otherwise"""
     return type(obj)== a_class
-#!/usr/bin/python3
-"""Module containing is_same_class method"""
-
-
-def is_same_class(obj, a_class):
-    """Returns:
-    True: if the object is exactly an instance of the specified class
-    False: otherwise"""
-    return type(obj) == a_class
