3ac7c844b223e65e69e1536c9df73c37a6faa877
marvcv <librarianmarvin@gmail.com>
2024-01-09 00:00:47 -0800

    inh

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" """
 def inherits_from(obj, a_class):
     """returns true if obj is instance of a class inh
     directly /indirectly otherwise false"""
-    return isinstance(obj, a_class) type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
