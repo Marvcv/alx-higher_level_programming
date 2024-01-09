a715770093e3f801346684823abab1e9c40c1a02
marvcv <librarianmarvin@gmail.com>
2024-01-09 00:05:00 -0800

    inh

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module containing inherits_from method"""
 
+
 def inherits_from(obj, a_class):
     """returns true if obj is instance of a class inh
     directly /indirectly otherwise false"""
