91c264739cddc97eab08260f56eaaab395d40fab
marvcv <librarianmarvin@gmail.com>
2024-02-15 10:09:14 -0800

    dba

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- List all te tables
+-- Query to list tables of a database
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Create a table in current database
+-- Query to create a table called first_table in the current database in mysql server
+CREATE TABLE IF NOT EXISTS first_name(
+id INT,
+name VARCHAR(256));
