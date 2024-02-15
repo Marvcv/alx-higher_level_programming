51d57fbd17a7c61b76a27c150f1723aee13db49f
marvcv <librarianmarvin@gmail.com>
2024-02-15 11:35:29 -0800

    rm

--- 0x0D-SQL_introduction/4-first_table.sql
+++
@@ -1,5 +0,0 @@
--- Create a table in current database
--- Query to create a table called first_table in the current database in mysql server
-CREATE TABLE IF NOT EXISTS first_name(
-id INT,
-name VARCHAR(256));
