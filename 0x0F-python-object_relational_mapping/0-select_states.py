a6ca3516853edeb8255ee207a9fa79bb3b63bac4
marvcv <librarianmarvin@gmail.com>
2024-03-22 00:47:23 -0700

    st

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+Lists all states from the database hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MYSQLdb.connect(
+    db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
