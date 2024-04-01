b0cdefd08fba3840c12d05eaf8e478fa4091bb99
marvcv <librarianmarvin@gmail.com>
2024-04-01 10:24:53 -0700

    size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a req to a URL and displays size of response
-curl -so /dev/null -w '%{size_download}\n' $1
+# script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
