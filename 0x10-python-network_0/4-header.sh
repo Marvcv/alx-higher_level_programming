f4b3687853fe3dc4465bd031950b7c3bc913b98c
marvcv <librarianmarvin@gmail.com>
2024-04-01 10:34:56 -0700

    hder

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends GET req with header var
-curl -s -H "X-School-User-Id:98" "$1"`
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -s "$1" -H "X-School-User-Id: 98"
